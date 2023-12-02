efbddf547ab1091f1db54b01efcde849a35f27f0
lucascstro <lukas.castro@live.com>
2023-12-02 16:31:31 -0300

    feito refatoração na chamadas do métodos do eventos.

--- ProEventos.Domain/Lote.cs
+++ ProEventos.Domain/Lote.cs
@@ -9,5 +9,5 @@ public class Lote
     public DateTime? DataFim { get; set; }
     public int Quantidade { get; set; }
     public int EventoId { get; set; }
-    public Evento Evento { get; set; }
+    public Evento? Evento { get; set; }
 }
